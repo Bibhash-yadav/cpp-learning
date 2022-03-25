368b0db234d70d0784130686959c86a96699152c
techxtor <techxtor472@gmail.com>
2022-03-26 00:31:39 +0545

    compound intrest

--- Easy program/compound-instreast.cpp
+++ Easy program/compound-instreast.cpp
@@ -1,23 +1,23 @@
-#include<iostream>
-#include<math.h>
- 
+#include <iostream>
+#include <math.h>
+
 using namespace std;
- 
+
 int main()
 {
-    float p,r,t,cp,ci;	
-	cout<<" Input the Principle = ";
-	cin>>p;
-	cout<<" Input the Rate = ";
-	cin>>r;
-	cout<<" Input the Time =  ";
-	cin>>t;	
-    ci=p*pow((1+r/100),t)-p;	
-    cp=p*pow((1+r/100),t);
-    cout<<endl;
-    cout<<" Compound Intrest = "<<ci;    
-    cout << endl; 
-	cout<<" compounded amount =  "<<cp;    
-    cout << endl; 
+    float p, r, t, cp, ci;
+    cout << " Input the Principle = ";
+    cin >> p;
+    cout << " Input the Rate = ";
+    cin >> r;
+    cout << " Input the Time =  ";
+    cin >> t;
+    ci = p * pow((1 + r / 100), t) - p;
+    cp = p * pow((1 + r / 100), t);
+    cout << endl;
+    cout << " Compound Intrest = " << ci;
+    cout << endl;
+    cout << " compounded amount =  " << cp;
+    cout << endl;
     return 0;
 }
