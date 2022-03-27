d4199393ccd2f931ed4d3ad030ad80cbf3aa7282
techxtor <techxtor472@gmail.com>
2022-03-27 20:24:13 +0545

    Date & time

--- date/date.cpp
+++ date/date.cpp
@@ -2,13 +2,11 @@
 #include <ctime>
 using namespace std;
 
-int main() 
+int main()
 {
-   // current date and time on the current system
    time_t now = time(0);
 
-   // convert now to string form
-   char* date_time = ctime(&now);
+   char *date_time = ctime(&now);
 
    cout << "The current date and time is: " << date_time << endl;
 }
