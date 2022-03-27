d566652642bc3be0e3aa53c58cdd973c329686b6
techxtor <techxtor472@gmail.com>
2022-03-27 20:30:42 +0545

    Date & time

---
+++ If-else-statements/if-else-statements.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+int age;
+cout<<"Enter your age"
+return 0;
+}
--- date/date.cpp
+++ date/date.cpp
@@ -4,9 +4,12 @@ using namespace std;
 
 int main()
 {
+   int a;
    time_t now = time(0);
 
    char *date_time = ctime(&now);
 
    cout << "The current date and time is: " << date_time << endl;
+   cout<<"Press any key to exit"<<endl;
+   cin>>a;
 }
