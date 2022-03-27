3e2a9a82420f2c08f4f235011666c2a291917340
techxtor <techxtor472@gmail.com>
2022-03-27 20:48:09 +0545

    If-else-statements

--- If-else-statements/if-else-statements.cpp
+++ If-else-statements/if-else-statements.cpp
@@ -3,6 +3,17 @@ using namespace std;
 
 int main(){
 int age;
-cout<<"Enter your age"
+cout<<"Enter your age"<<endl;
+cin>>age;
+if(age < 18){
+cout<<"You can't drive car "<<endl;
+}
+else if(age == 18)
+{
+    cout<<"You have to give some test"<<endl;
+}
+else{
+    cout<<"You can drive car"<<endl;
+}
 return 0;
 }
